3a3a9bbfe0f924c710c60412336cd56b6eb4d1e4
Sahoo, Akash K <akash_sahoo2@optum.com>
2021-09-27 17:42:38 +0530

    adding curd operations

--- springorm/src/main/java/com/spring/orm/dao/StudentDao.java
+++ springorm/src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -18,10 +20,33 @@ public class StudentDao {
 		this.hibernateTemplate = hibernateTemplate;
 	}
 
+//	save student
 	@Transactional
 	public int insert(Student student) {
 		Integer i = (Integer) this.hibernateTemplate.save(student);
 		return i;
+	}
+
+//	get the single data(object)
+	public Student getStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+
+//	get all students(all rows)
+	public List<Student> getAllStudents() {
+		return this.hibernateTemplate.loadAll(Student.class);
+	}
+
+//	delete student
+	public void deleteStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+	}
+
+//	update student
+	public void updateStudent(Student student) {
+		this.hibernateTemplate.update(student);
 	}
 
 }
